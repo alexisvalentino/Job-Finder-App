9c844a7ef92e053f5280c4e0cfb0a9641844a683
alexisvalentino <alexis01valentino@gmail.com>
2023-07-30 23:57:07 +0800

    Jsearch on Rapid API

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY
+
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, SetError] = useState(null);
@@ -10,9 +13,24 @@ const useFetch = () => {
         method: 'GET',
         url: 'https://jsearch.p.rapidapi.com/${endpoint}',
         headers: {
-            'X-RapidAPI-Key': 'f03db697admshe8fe8588452b631p17da17jsne0fc4f5815e8',
+            'X-RapidAPI-Key': rapidApiKey,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {query: 'Python Developer in Texas, USA', page: '1', num_pages: '1'},
+        params: { ...query },
     };
+
+    const fetchdata = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request
+            (options);
+
+            setData(response.data.data);
+        } catch (error) {
+
+        } finally {
+
+        }
+    }
 }
