016b995520684c35eff1ee07983f13732ba705f1
alexisvalentino <alexis01valentino@gmail.com>
2023-07-30 17:22:31 +0800

    JSearch RAPID API

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,4 +5,14 @@ const useFetch = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, SetError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: 'https://jsearch.p.rapidapi.com/${endpoint}',
+        headers: {
+            'X-RapidAPI-Key': 'f03db697admshe8fe8588452b631p17da17jsne0fc4f5815e8',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: {query: 'Python Developer in Texas, USA', page: '1', num_pages: '1'},
+    };
 }
